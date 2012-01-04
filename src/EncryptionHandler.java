a9cd10d51eadedb93ba6dbe99ca7ab1f957ee3cd
Eduard <eduard.thamm@thammit.at>
2012-01-04 12:54:55 +0100

    add IV handling and some comments

--- src/EncryptionHandler.java
+++ src/EncryptionHandler.java
@@ -1,6 +1,7 @@
 import java.security.*;
 
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 
 import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
 import com.sun.org.apache.xml.internal.security.utils.Base64;
@@ -12,26 +13,27 @@ public class EncryptionHandler {
     private Cipher enccipher;
     private Cipher deccipher;
     
-    
+    //Initialize a msg verifyer
     public EncryptionHandler(Key secret, String Algorithm) throws NoSuchAlgorithmException, InvalidKeyException{
         key = secret;
         hmac= Mac.getInstance(Algorithm);
         hmac.init(key);
     }
-    
-    public EncryptionHandler(Key secret, String Algorithm, byte[] IV) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException{
+    //initialize a cipher with an IV (i.e. AES in our case9
+    public EncryptionHandler(Key secret, String Algorithm, byte[] IV) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException{
             enccipher = Cipher.getInstance(Algorithm);
             deccipher = Cipher.getInstance(Algorithm);
-            //TODO IV
-            enccipher.init(Cipher.ENCRYPT_MODE, secret);
-            deccipher.init(Cipher.DECRYPT_MODE, secret);
+            IvParameterSpec ivp = new IvParameterSpec(IV);
+            enccipher.init(Cipher.ENCRYPT_MODE, secret, ivp);
+            deccipher.init(Cipher.DECRYPT_MODE, secret, ivp);
     }
+    //init a chipher w/o iv but with 2 keys RSA-PSK in our case
     public EncryptionHandler(Key enc, Key dec, String Algorithm) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException{
         enccipher = Cipher.getInstance(Algorithm);
         deccipher = Cipher.getInstance(Algorithm);
         enccipher.init(Cipher.ENCRYPT_MODE, enc);
         deccipher.init(Cipher.DECRYPT_MODE, dec);
-}
+    }
     
     public String encryptMessage(String msg) throws IllegalBlockSizeException, BadPaddingException{
         String b64 = Base64.encode(msg.getBytes());
@@ -46,6 +48,8 @@ public class EncryptionHandler {
         String clear = new String(Base64.decode(b64));
         return clear;
     }
+    
+    
     
     public byte[] generateIntegrityCheck(byte[] msg){
         hmac.update(msg);
